b73b239494c11cf49de8b0fba9a96655d207ba8e
zn2211 <zn2211@barnard.edu>
2025-12-07 18:58:42 -0500

    Update server.py
    scoreboard = sorted(scoreboard, key=lambda t: t["score"], reverse=True)

--- server.py
+++ server.py
@@ -51,6 +51,8 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+        scoreboard = sorted(scoreboard, key=lambda t: t["score"], reverse=True)
+
     return jsonify(scoreboard=scoreboard)
 
 
